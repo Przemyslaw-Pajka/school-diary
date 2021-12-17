5747d137cf57efac42aa95c7af69d1bc6232cc99
Przemysław Pajka <przemyslaw.pajka94@gmail.com>
2021-12-17 17:50:06 +0100

    remove unused showPrompt in Admin.js

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,13 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { FormRegistration } from './FormRegistration/FormRegistration';
-import { showPrompt } from '../utilities/showPrompt';
 
 export const Admin = () => {
     const [isCorrectAdminPassword, setIsCorrectAdminPassword] = useState(true);
 
-    useEffect(() => {
-        // showPrompt(setIsCorrectAdminPassword)
-    }, [])
 
     return (
         <>
